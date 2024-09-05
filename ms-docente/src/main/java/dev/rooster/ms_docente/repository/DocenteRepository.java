e86bd794373003d95a009ee84ee41fabc0aab951
EnriqueMV18 <enriquemv937@gmail.com>
2024-09-04 22:20:15 -0500

    v0.1 (Configuracion Centralizada

--- ms-docente/src/main/java/dev/rooster/ms_docente/repository/DocenteRepository.java
+++
@@ -1,9 +0,0 @@
-package dev.rooster.ms_docente.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import dev.rooster.ms_docente.entity.Docente;
-
-public interface DocenteRepository extends JpaRepository<Docente, Integer>{
-    
-}
