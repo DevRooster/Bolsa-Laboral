67052ec6aa8d71a57adf89b7d071bf018f90e868
EnriqueMV18 <enriquemv937@gmail.com>
2024-09-06 23:15:29 -0500

    v0.4.1(Update de archivos)

--- ms-gestion_estudiantes/src/main/java/com/example/msgestion_estudiantes/entity/Estudiante.java
+++ ms-gestion_estudiantes/src/main/java/com/example/msgestion_estudiantes/entity/Estudiante.java
@@ -1,11 +1,7 @@
 package com.example.msgestion_estudiantes.entity;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
 import lombok.Data;
-
-import java.time.LocalDate;
-import java.util.List;
 
 @Entity
 @Data
@@ -14,14 +10,9 @@ public class Estudiante {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String nombre;
-    private LocalDate fechaNacimiento;
-    private String DNI;
-    private String direccion;
-    private String telefono;
-    private String email;
-    private String gradoActual;
-
-    @JsonIgnoreProperties({"hibernateLazyInitializer", "handler", "estudiante"})
-    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL, mappedBy = "estudiante")
-    private List<Historial_Academico> historialAcademicos;
+    private String apellido;
+    private String carrera;
+    private String universidad;
+    private String habilidades;
+    private String experiencia;
 }
