35ffc2c7052ed7d65af5cf90f7bc02337b2ce89b
EnriqueMV18 <enriquemv937@gmail.com>
2024-09-06 23:09:49 -0500

    v0.4(Update de archivos)

--- ms-empresa/src/main/java/dev/rooster/ms_empresa/repository/EmpresaRespository.java
+++
@@ -1,9 +0,0 @@
-package dev.rooster.ms_empresa.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import dev.rooster.ms_empresa.entity.Empresa;
-
-public interface EmpresaRespository  extends JpaRepository<Empresa, Integer>{
-    
-}
--- ms-estudiante/src/main/java/dev/rooster/ms_estudiante/repository/EstudianteRepository.java
+++
@@ -1,9 +0,0 @@
-package dev.rooster.ms_estudiante.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import dev.rooster.ms_estudiante.entity.DatPersonales;
-
-public interface EstudianteRepository extends JpaRepository<DatPersonales, Integer>{
-    
-}
