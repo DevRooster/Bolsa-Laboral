a15d71d80590a18cc42896145eb13263f399390c
EnriqueMV18 <enriquemv937@gmail.com>
2024-09-08 23:36:38 -0500

    v0.11(Delete MS Documento)

--- ms-gestion_documento/src/main/java/com/example/msgestion_documento/repository/DocumentoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.msgestion_documento.repository;
-
-
-import com.example.msgestion_documento.entity.Documento;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DocumentoRepository extends JpaRepository <Documento, Integer> {
-}
