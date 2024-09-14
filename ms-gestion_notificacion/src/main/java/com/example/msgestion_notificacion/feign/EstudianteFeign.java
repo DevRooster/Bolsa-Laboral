376b85886becf67f916d6b206c3586fb0ab36c60
EnriqueMV18 <enriquemv937@gmail.com>
2024-09-14 16:40:29 -0500

    v0.18(Resiliencia Oferta < Empresa)

--- ms-gestion_notificacion/src/main/java/com/example/msgestion_notificacion/feign/EstudianteFeign.java
+++ ms-gestion_notificacion/src/main/java/com/example/msgestion_notificacion/feign/EstudianteFeign.java
@@ -6,8 +6,21 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import com.example.msgestion_notificacion.dto.EstudianteDto;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+
 @FeignClient(name = "ms-gestionestudiantes-service",path = "/estudiante")
 public interface EstudianteFeign {
     @GetMapping("/{id}")
+    @CircuitBreaker(name = "estudianteListarPorIdCB", fallbackMethod = "fallbackMethod")
     EstudianteDto buscarPorId(@PathVariable("id") Integer id);
+
+    // Método de fallback
+    default EstudianteDto fallbackMethod(Integer id, Throwable throwable) {
+        // Devuelve un objeto EstudianteDto con valores predeterminados
+        EstudianteDto estudianteFallback = new EstudianteDto();
+        estudianteFallback.setId(id);
+        estudianteFallback.setNombre("Estudiante no disponible");
+        // Establece otros valores predeterminados según tu estructura de EstudianteDto
+        return estudianteFallback;
+    }
 }
