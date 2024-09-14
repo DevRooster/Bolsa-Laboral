2c9b65c7dacf9e1c72fc9fc58824aae61f5eed6b
EnriqueMV18 <enriquemv937@gmail.com>
2024-09-14 18:08:16 -0500

    v0.20(Resiliencia Seguimiento < Estudiante < Oferta < Empresa)

--- ms-gestion_seguimiento/src/main/java/com/example/msgestion_seguimiento/feign/EmpresaFeign.java
+++ ms-gestion_seguimiento/src/main/java/com/example/msgestion_seguimiento/feign/EmpresaFeign.java
@@ -4,9 +4,21 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import com.example.msgestion_seguimiento.dto.EmpresaDto;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 
 @FeignClient(name = "ms-gestionempresa-service",path = "/empresa")
 public interface EmpresaFeign {
+    @CircuitBreaker(name = "empresaListarPorIdCB", fallbackMethod = "fallbackMethod")
     @GetMapping("/{id}")
     EmpresaDto buscarPorId(@PathVariable("id") Integer id);
+
+    // Método de fallback
+    default EmpresaDto fallbackMethod(Integer id, Throwable throwable) {
+        // Devuelve un objeto EstudianteDto con valores predeterminados
+        EmpresaDto empresaFallback = new EmpresaDto();
+        empresaFallback.setId(id);
+        empresaFallback.setNombre("Estudiante no disponible");
+        // Establece otros valores predeterminados según tu estructura de EstudianteDto
+        return empresaFallback;
+    }
 }
