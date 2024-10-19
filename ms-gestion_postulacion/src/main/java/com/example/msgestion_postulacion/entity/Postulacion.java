62abfb2eabb64987f375f96469d6b299708ae18b
EnriqueMV18 <enriquemv937@gmail.com>
2024-10-19 16:15:19 -0500

    Organizacion de data

--- ms-gestion_postulacion/src/main/java/com/example/msgestion_postulacion/entity/Postulacion.java
+++ ms-gestion_postulacion/src/main/java/com/example/msgestion_postulacion/entity/Postulacion.java
@@ -15,16 +15,16 @@ public class Postulacion {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    private Integer estudianteId;  // Referencia al ID de Estudiante
-    private Integer ofertaId;      // Referencia al ID de Oferta
-    
     private String estadoPostulacion; // EN_REVISION, ACEPTADO, RECHAZADO
-
+    
     private LocalDate fechaPostulacion;
-
+    
+    private Integer estudianteId;  // Referencia al ID de Estudiante
+    
     @Transient  // No se guarda en la base de datos
     private EstudianteDto estudianteDto;  // DTO de Estudiante para cargar los datos desde ms-gestion_estudiantes
-
+    
+    private Integer ofertaId;      // Referencia al ID de Oferta
     @Transient  // No se guarda en la base de datos
     private OfertaDto ofertaDto;  // DTO de Oferta para cargar los datos desde ms-gestion_oferta
 }
